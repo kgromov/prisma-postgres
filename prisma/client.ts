e15a97af9daadd0527e761bee40ccd71553ed004
konstantin <konst.gromov@gmail.com>
2026-01-19 02:37:43 +0200

    Use sqlite as Prisma ORM datasource

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,10 +1,10 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = `${process.env.SQLITE_CONNECTION_URL}`;
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const prisma = new PrismaClient({ adapter });
 
-export { prisma }
+export { prisma };
